7c4dacb96fb5eed2bbb77cbf6572a0868b0deebe
chungwon <wjdcjddnjs1234@gmail.com>
2025-01-02 09:46:39 +0900

    4.나이 출력

---
+++ 4_test.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int age) {
+        int a = age-1;
+        int b = 2022;
+        if(a == 40){
+            return b-a;
+        } else{
+            return b-a;
+        }
+    }
+}
