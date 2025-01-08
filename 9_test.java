9ae3f203b1f97755d8447417a590a82f4a702906
chungwonJ <wjdcjddnjs1234@gmail.com>
2025-01-08 09:28:15 +0900

    9.짝수의 합

--- 9_test.java
+++ 9_test.java
@@ -1,19 +1,13 @@
-#include <stdio.h>
-#include <stdbool.h>
-#include <stdlib.h>
+class Solution {
+    public static int solution(int n) {
+        int answer = 0;
 
-int solution(int n) {
-    int answer = 0;
-    if(0<n && n<=1000){
-        for (int i = 2; i <= n; i += 2) {
-            answer += i;
+        if (0 < n && n <= 1000) {
+            for (int i = 2; i <= n; i += 2) {
+                answer += i;
+            }
         }
-    }
-    return answer;
-}
 
-int main() {
-    int n = 10; 
-    int result = solution(n);
-    return 0;
+        return answer;
+    }
 }
